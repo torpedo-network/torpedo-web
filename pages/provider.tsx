e1da4f62e764a8d8bc34dedf768a28f626cd1737
Anthony Zhou <37421108+anthony-zhou@users.noreply.github.com>
2023-01-26 09:33:10 -0500

    Rename provider component

--- pages/provider.tsx
+++ pages/provider.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import AppLayout from "../components/AppLayout";
 
-export default function Home() {
+export default function Provider() {
   return (
     <AppLayout>
       <Head>
